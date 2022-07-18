65e8f5bb68f6619861d49a514bc12191cbd113fe
BertrandD <volubyl@pm.me>
2022-07-18 12:13:14 +0200

    fix: type checking

--- tests/scripts/test_check_organization.py
+++ tests/scripts/test_check_organization.py
@@ -4,5 +4,10 @@ from scripts.check_organizations import main
 
 
 def test_return_0_exit_code_if_all_organizations_have_the_right_format() -> None:
-    code = main(Path("tests/fixtures"))
+    code = main(Path("tests/fixtures/with_well_formatted_organizations"))
     assert code == 0
+
+
+def test_return_1_exit_code_if_all_organizations_have_not_the_right_format() -> None:
+    code = main(Path("tests/fixtures/with_badly_formatted_organizations"))
+    assert code == 1
