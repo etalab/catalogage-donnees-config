deeac69f1b1ee1fb04fbf69594bdeea0eedb3c5b
BertrandD <volubyl@pm.me>
2022-08-11 13:45:45 +0200

    refacto: make get_organizations_path more generic

---
+++ lib/common.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import List
+
+
+def get_path(org_dir_path: Path, file_name: str) -> List[Path]:
+    org_paths: List[Path] = []
+    for orgdir in Path(org_dir_path).iterdir():
+        if not orgdir.is_dir() and orgdir.name == ".gitkeep":
+            continue
+
+        org_path = orgdir / file_name
+        org_paths.append(org_path)
+
+    # Order of iterdir() is not deterministic.
+    # Force it for cross-platform testing purposes.
+    org_paths = sorted(org_paths, key=lambda p: p.relative_to(org_dir_path))
+
+    return org_paths
