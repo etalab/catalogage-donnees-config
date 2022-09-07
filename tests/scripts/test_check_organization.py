fb61526365f5f49612dbb0b920bc1ba98cff03fb
Volubyl <volubyl@pm.me>
2022-09-07 11:44:26 +0200

    fix: add more tests to improve code coverage (#19)

--- tests/scripts/test_check_organization.py
+++ tests/scripts/test_check_organization.py
@@ -8,6 +8,11 @@ def test_return_0_exit_code_if_all_organizations_have_the_right_format() -> None
     assert code == 0
 
 
-def test_return_1_exit_code_if_all_organizations_have_not_the_right_format() -> None:
-    code = main(Path("tests/fixtures/with_badly_formatted_organizations"))
+def test_return_1_exit_code_if_a_property_is_missing_in_a_organization_file() -> None:
+    code = main(Path("tests/fixtures/with_missing_organization_properties"))
+    assert code == 1
+
+
+def test_return_1_exit_code_if_an_organization_file_contains_more_than_one_organization() -> None:  # noqa: E501
+    code = main(Path("tests/fixtures/with_more_than_one_organization_in_a_single_file"))
     assert code == 1
