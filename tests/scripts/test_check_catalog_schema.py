8f1e031a9ea5b0be006c5582b5c06f7fa00d2652
volubyl <volubyl@pm.me>
2022-10-17 15:24:46 +0200

    fix: skip catalog checks if catalog does not exists

--- tests/scripts/test_check_catalog_schema.py
+++ tests/scripts/test_check_catalog_schema.py
@@ -21,9 +21,9 @@ def test_return_1_exit_code_if_all_catalog_schema_are_not_valid_table_schema() -
     assert code == 1
 
 
-def test_return_1_exit_code_if_a_catalog_schema_does_not_exists() -> None:
+def test_return_0_exit_code_if_a_catalog_schema_does_not_exists() -> None:
     code = main(Path("tests/fixtures/with_no_existing_catalog_schema"))  # noqa: E501
-    assert code == 1
+    assert code == 0
 
 
 def test_return_1_exit_code_if_at_least_one_field_is_missing() -> None:
